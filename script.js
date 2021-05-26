431c2d543226fe9606820d8457d9b23a82111c8c
mauradolphin <maura.anne.lenahan@gmail.com>
2021-05-26 10:15:24 -0500

    Cleaned JS Functions

--- script.js
+++ script.js
@@ -1,80 +1,83 @@
 /** @format */
 function showCelciusForecast(response) {
 	let dayOneHigh = Math.round(response.data.daily[0].temp.max);
-	document.querySelector("#day-1-temp-high").innerHTML = `${dayOneHigh}°C`;
 	let dayOneLow = Math.round(response.data.daily[0].temp.min);
-	document.querySelector("#day-1-temp-low").innerHTML = `${dayOneLow}°C`;
 	let dayTwoHigh = Math.round(response.data.daily[1].temp.max);
-	document.querySelector("#day-2-temp-high").innerHTML = `${dayTwoHigh}°C`;
 	let dayTwoLow = Math.round(response.data.daily[1].temp.min);
-	document.querySelector("#day-2-temp-low").innerHTML = `${dayTwoLow}°C`;
 	let dayThreeHigh = Math.round(response.data.daily[2].temp.max);
-	document.querySelector("#day-3-temp-high").innerHTML = `${dayThreeHigh}°C`;
 	let dayThreeLow = Math.round(response.data.daily[2].temp.min);
-	document.querySelector("#day-3-temp-low").innerHTML = `${dayThreeLow}°C`;
 	let dayFourHigh = Math.round(response.data.daily[3].temp.max);
-	document.querySelector("#day-4-temp-high").innerHTML = `${dayFourHigh}°C`;
 	let dayFourLow = Math.round(response.data.daily[3].temp.min);
-	document.querySelector("#day-4-temp-low").innerHTML = `${dayFourLow}°C`;
 	let dayFiveHigh = Math.round(response.data.daily[4].temp.max);
-	document.querySelector("#day-5-temp-high").innerHTML = `${dayFiveHigh}°C`;
 	let dayFiveLow = Math.round(response.data.daily[4].temp.min);
+
+	document.querySelector("#day-1-temp-high").innerHTML = `${dayOneHigh}°C`;
+	document.querySelector("#day-1-temp-low").innerHTML = `${dayOneLow}°C`;
+	document.querySelector("#day-2-temp-high").innerHTML = `${dayTwoHigh}°C`;
+	document.querySelector("#day-2-temp-low").innerHTML = `${dayTwoLow}°C`;
+	document.querySelector("#day-3-temp-high").innerHTML = `${dayThreeHigh}°C`;
+	document.querySelector("#day-3-temp-low").innerHTML = `${dayThreeLow}°C`;
+	document.querySelector("#day-4-temp-high").innerHTML = `${dayFourHigh}°C`;
+	document.querySelector("#day-4-temp-low").innerHTML = `${dayFourLow}°C`;
+	document.querySelector("#day-5-temp-high").innerHTML = `${dayFiveHigh}°C`;
 	document.querySelector("#day-5-temp-low").innerHTML = `${dayFiveLow}°C`;
 }
 
 function showForecast(response) {
 	let dayOneHigh = Math.round(response.data.daily[0].temp.max);
-	document.querySelector("#day-1-temp-high").innerHTML = `${dayOneHigh}°F`;
 	let dayOneLow = Math.round(response.data.daily[0].temp.min);
-	document.querySelector("#day-1-temp-low").innerHTML = `${dayOneLow}°F`;
 	let dayTwoHigh = Math.round(response.data.daily[1].temp.max);
-	document.querySelector("#day-2-temp-high").innerHTML = `${dayTwoHigh}°F`;
 	let dayTwoLow = Math.round(response.data.daily[1].temp.min);
-	document.querySelector("#day-2-temp-low").innerHTML = `${dayTwoLow}°F`;
 	let dayThreeHigh = Math.round(response.data.daily[2].temp.max);
-	document.querySelector("#day-3-temp-high").innerHTML = `${dayThreeHigh}°F`;
 	let dayThreeLow = Math.round(response.data.daily[2].temp.min);
-	document.querySelector("#day-3-temp-low").innerHTML = `${dayThreeLow}°F`;
 	let dayFourHigh = Math.round(response.data.daily[3].temp.max);
-	document.querySelector("#day-4-temp-high").innerHTML = `${dayFourHigh}°F`;
 	let dayFourLow = Math.round(response.data.daily[3].temp.min);
-	document.querySelector("#day-4-temp-low").innerHTML = `${dayFourLow}°F`;
 	let dayFiveHigh = Math.round(response.data.daily[4].temp.max);
-	document.querySelector("#day-5-temp-high").innerHTML = `${dayFiveHigh}°F`;
 	let dayFiveLow = Math.round(response.data.daily[4].temp.min);
+
+	document.querySelector("#day-1-temp-high").innerHTML = `${dayOneHigh}°F`;
+	document.querySelector("#day-1-temp-low").innerHTML = `${dayOneLow}°F`;
+	document.querySelector("#day-2-temp-high").innerHTML = `${dayTwoHigh}°F`;
+	document.querySelector("#day-2-temp-low").innerHTML = `${dayTwoLow}°F`;
+	document.querySelector("#day-3-temp-high").innerHTML = `${dayThreeHigh}°F`;
+	document.querySelector("#day-3-temp-low").innerHTML = `${dayThreeLow}°F`;
+	document.querySelector("#day-4-temp-high").innerHTML = `${dayFourHigh}°F`;
+	document.querySelector("#day-4-temp-low").innerHTML = `${dayFourLow}°F`;
+	document.querySelector("#day-5-temp-high").innerHTML = `${dayFiveHigh}°F`;
 	document.querySelector("#day-5-temp-low").innerHTML = `${dayFiveLow}°F`;
 }
 
 function showData(response) {
 	console.log(response);
 	let humidity = document.querySelector("#humidity");
-	humidity.innerHTML = response.data.main.humidity;
 	let currentTemp = Math.round(response.data.main.temp);
 	let currentTempDisplay = document.querySelector("#current-temp");
-	currentTempDisplay.innerHTML = currentTemp;
 	let newTempHigh = Math.round(response.data.main.temp_max);
 	let temperatureHigh = document.querySelector("#temp-high");
-	temperatureHigh.innerHTML = `${newTempHigh}°F`;
 	let tempLow = Math.round(response.data.main.temp_min);
 	let temperatureLow = document.querySelector("#temp-low");
-	temperatureLow.innerHTML = `${tempLow}°F`;
-	document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 	let sunriseTime = response.data.sys.sunrise;
 	let sunrise = new Date(sunriseTime * 1000);
 	let timeString = sunrise.toLocaleTimeString();
-	document.querySelector("#sunrise-time-display").innerHTML = timeString;
 	let sunsetTime = response.data.sys.sunset;
 	let sunset = new Date(sunsetTime * 1000);
 	let setTimeString = sunset.toLocaleTimeString();
-	document.querySelector("#sunset-time-display").innerHTML = setTimeString;
 	let city = response.data.name;
-	city = city.toLowerCase();
 	let latitude = response.data.coord.lat;
 	let longitude = response.data.coord.lon;
 	let apiEndpoint = `https://api.openweathermap.org/data/2.5/onecall`;
 	let units = "imperial";
 	let apiKey = "52e52eb6f8e287f91bec28fc7ec32f3b";
 	let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&exclude=currently,minutely,hourly,alerts&appid=${apiKey}&units=${units}`;
+
+	humidity.innerHTML = response.data.main.humidity;
+	currentTempDisplay.innerHTML = currentTemp;
+	temperatureHigh.innerHTML = `${newTempHigh}°F`;
+	temperatureLow.innerHTML = `${tempLow}°F`;
+	document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+	document.querySelector("#sunrise-time-display").innerHTML = timeString;
+	document.querySelector("#sunset-time-display").innerHTML = setTimeString;
+	city = city.toLowerCase();
 	axios.get(apiUrl).then(showForecast);
 }
 
@@ -162,32 +165,34 @@ function clickFahrenheit(event) {
 function showLocalData(response) {
 	let currentTemp = Math.round(response.data.main.temp);
 	let currentTempDisplay = document.querySelector("#current-temp");
-	currentTempDisplay.innerHTML = currentTemp;
 	let tempHigh = Math.round(response.data.main.temp_max);
 	let temperatureHigh = document.querySelector("#temp-high");
-	temperatureHigh.innerHTML = `${tempHigh}°F`;
 	let tempLow = Math.round(response.data.main.temp_min);
 	let temperatureLow = document.querySelector("#temp-low");
-	temperatureLow.innerHTML = `${tempLow}°F`;
 	let city = response.data.name;
 	let currentCityDisplay = document.querySelector("#current-city");
-	currentCityDisplay.innerHTML = city;
-	document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 	let sunriseTime = response.data.sys.sunrise;
 	let sunrise = new Date(sunriseTime * 1000);
 	let timeString = sunrise.toLocaleTimeString();
-	document.querySelector("#sunrise-time-display").innerHTML = timeString;
 	let sunsetTime = response.data.sys.sunset;
 	let sunset = new Date(sunsetTime * 1000);
 	let setTimeString = sunset.toLocaleTimeString();
-	document.querySelector("#sunset-time-display").innerHTML = setTimeString;
-	city = city.toLowerCase();
 	let latitude = response.data.coord.lat;
 	let longitude = response.data.coord.lon;
 	let apiEndpoint = `https://api.openweathermap.org/data/2.5/onecall`;
 	let units = "imperial";
 	let apiKey = "52e52eb6f8e287f91bec28fc7ec32f3b";
 	let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&exclude=current.minutelyt,hourly,alerts&appid=${apiKey}&units=${units}`;
+
+	currentTempDisplay.innerHTML = currentTemp;
+	temperatureHigh.innerHTML = `${tempHigh}°F`;
+	temperatureLow.innerHTML = `${tempLow}°F`;
+	city = city.toLowerCase();
+	currentCityDisplay.innerHTML = city;
+	document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+	document.querySelector("#sunrise-time-display").innerHTML = timeString;
+	document.querySelector("#sunset-time-display").innerHTML = setTimeString;
+
 	axios.get(apiUrl).then(showForecast);
 }
 
@@ -210,6 +215,7 @@ function pageLoad() {
 	let apiKey = "52e52eb6f8e287f91bec28fc7ec32f3b";
 	let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
 	currentCityDisplay.innerHTML = city;
+
 	axios.get(apiUrl).then(showData);
 }
 
